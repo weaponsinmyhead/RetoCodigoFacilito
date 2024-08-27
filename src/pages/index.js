71dde9c7704456f00ede4cf85ff8bf2b5995ef98
weaponsinmyhead <64826226+weaponsinmyhead@users.noreply.github.com>
2024-08-27 19:35:19 +0000

    add: hours

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import dbConnect from "../lib/dbConnect";
 import Pet from "../models/Pet";
-
+import Hours from '../components/Hours'
 // TODO: Import Hours component
 
 const Index = ({ pets }) => {
@@ -10,7 +10,7 @@ const Index = ({ pets }) => {
     <>
 
       {/* TODO: Display Hours component */}
-
+      <Hours />
       {/* Create a card for each pet */}
       {pets.map((pet) => (
         <div key={pet._id}>
